eb37d0f14b129e6d85ae62090f22ff438c8c37a9
Ridhuwan <xros2600@gmail.com>
2024-03-28 15:17:34 +0800

    CRUD

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 })
 
 //Path: package.json
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://B022210140:ridhuwan17@ridhuwan17.y6ssjvq.mongodb.net/?retryWrites=true&w=majority&appName=Ridhuwan17";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,13 +28,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // insert a document into the database
+    //let result = await client.db ('maybank2u').collection('students').insertOne({
+      //name: 'Ridhuwan' ,
+      //age : 25,
+      //status: 'A',
+      //faculty: 'FTKEK'
+    //});
+
+      let result = await client.db ('maybank2u').collection('students').findOne(
+        {
+          name: 'Ahmad'
+        }
+        //{ _id: new ObjectId("660511dd96a7f358b46eca40") },
+        //{ $set: { name: 'Ahmad' } }
+      ).toArray()        
+    console.log(result)
+
+
+    //console.log("You successfully connected to MongoDB!GOOD JOB BRO!" + result.insertedId)
+
+    //let result = await client.db ('maybank2u').collection('students').find().toArray();
+    //console.log(result);
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
