31b0e7539c6e1b03cfd8a453ac4102cee4dc4713
Ridhuwan <xros2600@gmail.com>
2024-06-07 15:17:26 +0800

    progress

--- python_assignment/salesCounter_module.py
+++ python_assignment/salesCounter_module.py
@@ -3,18 +3,15 @@ from datetime import datetime
 
 sales_file = "daily_sales.txt"
 previous_sales_file = "previous_sales.txt"
-total_profit = 0.0
 
-def update_sales(amount):
-    global total_profit
-    total_profit += amount
+def update_sales(amount, username):
     with open(sales_file, "a") as file:
         date_today = datetime.now().strftime("%d/%m/%Y %I:%M%p")
-        file.write(f"{date_today} Total Sales: RM{amount}\n")
-    print(f"Current Total Profit: RM{total_profit:.2f}")
+        file.write(f"{date_today} - {username}'s Order: RM{amount}\n")
+    
+    update_total_sales()
 
 def view_sales():
-    global total_profit
     if not os.path.exists(sales_file):
         print("No sales recorded for today.")
         return
@@ -22,12 +19,10 @@ def view_sales():
     with open(sales_file, "r") as file:
         sales = file.readlines()
     
-    total_sales = sum(float(sale.split('RM')[-1]) for sale in sales)
+    total_sales = sum(float(sale.split('RM')[-1]) for sale in sales if "Total Sales" not in sale)
     print(f"Total sales for the day: RM{total_sales:.2f}")
-    print(f"Current Total Profit: RM{total_profit:.2f}")
 
 def reset_sales():
-    global total_profit
     if os.path.exists(sales_file):
         with open(sales_file, "r") as file:
             previous_sales = file.read()
@@ -37,14 +32,15 @@ def reset_sales():
         
         os.remove(sales_file)
         print("Sales counter reset and previous sales stored.")
-
-        # Update the total profit with previous sales
-        with open(previous_sales_file, "r") as file:
-            previous_sales = file.readlines()
-        
-        previous_total_sales = sum(float(sale.split('RM')[-1]) for sale in previous_sales)
-        total_profit = previous_total_sales
-
-        print(f"Updated Total Profit from previous sales: RM{total_profit:.2f}")
     else:
         print("No sales to reset.")
+
+def update_total_sales():
+    if os.path.exists(sales_file):
+        with open(sales_file, "r") as file:
+            sales = file.readlines()
+        
+        total_sales = sum(float(sale.split('RM')[-1]) for sale in sales if "Total Sales" not in sale)
+
+        with open(sales_file, "a") as file:
+            file.write(f"Total Sales: RM{total_sales:.2f}\n")
