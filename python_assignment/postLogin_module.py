4f091d0fb7526456b38d05606256938b6d920e94
Ridhuwan <xros2600@gmail.com>
2024-06-14 17:39:34 +0800

    new

--- python_assignment/postLogin_module.py
+++ python_assignment/postLogin_module.py
@@ -2,10 +2,12 @@ import menuPrice
 import adminSales
 import ast
 
+#to store all the users' order
 user_orders = {}
+#indicates how much money the user has to pay in cart
 money = 0
-totalPricetoPay = 0
 
+#function for logging in
 def userOption(username):
     print("""
         1. Order Drinks
@@ -13,23 +15,33 @@ def userOption(username):
         3. Update User Info
         4. Log Out
     """)
+    #prompts user to enter thier choice to order,view and etc
     choice1 = int(input("Enter your choice(e.g 1): "))
+    #passes the users' choice to the user_choice function 
     user_choice(username, choice1)
 
+#function for user choice
 def user_choice(username, choice):
-    global totalPricetoPay
+    #order drinks
     if choice == 1:
+        #prints the menu of the drinks
         menuPrice.menu_boba()
+        #stores all the details of the users' order
         order = menuPrice.calculate_price(money)
+        #making sure that the order belongs to the current user
         user_orders[username] = order
-
+        #update the daily sales of DaBubble Tea Shop (networth of the shop)
         adminSales.update_sales(order['total_price'])
+        #returns back to the users' page after ordering
         return userOption(username)
+    #view orders
     elif choice == 2:
+        #checks if the current orders doesnt contain any order
         order = user_orders.get(username, 'No orders found')
         if order == 'No orders found':
             print(order)
         else:
+            #if there is an order, print out the order
             print("Your order: {")
             for detail in order['order_details']:
                 print("    orders: [{")
@@ -38,62 +50,105 @@ def user_choice(username, choice):
                 print(f"        quantity: {detail['quantity']}")
                 print("    }]")
             print("},",end="")
-            print(f"Total price: {order['total_price']}")
-        return userOption(username)
+            print(f"Total price: RM{order['total_price']}")
+            
+            print('''\n1.Keep Ordering\n2.Pay\n3.Back\n''')
+            decision = int(input("Enter your choice:"))
+
+            if decision == 1:
+                return userOption(username)
+            elif decision == 2:
+                menuPrice.paymentSection(order['order_details'], order['total_price'])
+                order = []
+                menuPrice.clearPurchaseLog()
+                return userOption(username)
+            elif decision == 3:
+                print("Returning to the user's page...")
+                return userOption(username)
+                
+            else:
+                print("Invalid choice. Please try again.")
+                return userOption(username)
+    #update user info
     elif choice == 3:
+        #directs the user to the updating users' info page
         update_user_info(username)
         return userOption(username)
+    #log out (completely exit the program)
     elif choice == 4:
         print("Thank you for coming to the DaBubble Tea Shop")
         exit()
 
+#update user info function
 def update_user_info(username):
     print("""
     1. Update Name
     2. Update Password
     3. Update Age
     4. Update Email
+
+    Enter '0' to go back to the user page
+          
     """)
     choice = int(input("Enter your choice: "))
+
+    #go back to the user page if the user wishes to do so
+    if choice == 0:
+        userOption(username)
+
+    #update name
     if choice == 1:
         new_name = input("Enter your new name: ")
+        #function to change name specifically
         change_user_detail(username, 'username', new_name)
+        #after updating the name, return to the user page
         return userOption(new_name)
-    
+    #update password
     elif choice == 2:
         new_password = input("Enter your new password: ")
+        #function to change password specifically
         change_user_detail(username, 'password', new_password)
+        #after updating the password, return to the user page
         return userOption(username)
-    
+    #update age
     elif choice == 3:
         new_age = int(input("Enter your new age: "))
+        #function to change age specifically
         change_user_detail(username, 'age', new_age)
+        #after updating the age, return to the user page
         return userOption(username)
-    
+    #update email
     elif choice == 4:
         new_email = input("Enter your new email: ")
+        #function to change email specifically
         change_user_detail(username, 'email', new_email)
+        #after updating the email, return to the user page
         return userOption(username)
     
     else:
-        print("Invalid choice")
+        #if the user input is not valid then retry again until complete
+        print("Invalid choice. Please try again")
+        update_user_info(username)
 
 def change_user_detail(username, detail, new_value):
-
+    #reads the userList.txt file
     with open('userList.txt', 'r') as file:
         lines = file.readlines()
 
+    #parses the lines into a list of dictionaries
     users = [ast.literal_eval(line.strip()) for line in lines]
 
+    #checks if the new username already exists
     if detail == 'username' and any(user['username'] == new_value for user in users):
-        print(f"Username {new_value} already exists.")
+        print(f"Username {new_value} already exists. Please try again.")
+        #goes back to the user update page
         return update_user_info(username)
-
+    #if the new username doesn't exists, then update the details
     for user in users:
         if user['username'] == username:
             user[detail] = new_value
             break
-
+    #overwrite the specific user's details in the userList.txt file
     with open('userList.txt', 'w') as file:
         for user in users:
             file.write(str(user) + '\n')
--- python_assignment/promo.py
+++ python_assignment/promo.py
@@ -1,56 +1,60 @@
+import menuPrice
 
-promo = {"JIMAT": 15, "BOBATIME" : 20, "HAPPYDAY" : 25, "MYBOBA" : 30}
+#list of promo code
+promo = {"JIMAT": 15, "DABOBA" : 20, "HAPPY" : 25, "MYBOBA" : 30}
+#initializing the variables
+user_code = '0'
+total_price = 0
 
+#prints out the available promo code
 def printPromo():
     for i, j in zip(promo.keys(), promo.values()):
         print(f"Promo code: {i}, Discount: {j}%")
+    print()
 
-def applyPromo(user_code,total_price):
+#function to apply promo code
+def applyPromo(total_price):
+    #prints the available promo code
+    printPromo()
+    #prompts user to enter promo code (upper is used to capitalize all input)
+    user_code = input("Enter promo code ('no' to skip): ").upper()
 
+    #if user code is no, then no promo is applied
     if user_code == 'NO':
         print(f"TOTAL: RM{total_price:.2f}")
         print('Proceed to payment')
-        paymentMethod(total_price)
+        print("Order confirmed! Thank you for your order.")
+        print("Please pay at the counter.")
         return total_price
-
-    elif user_code in promo:
+    #checks if promo code is valid
+    if user_code in promo:
         discount = promo[user_code]
+        #calculating the new price of the drinks
         newPrice = total_price - (total_price * (discount / 100))
         print(f"TOTAL: RM{newPrice:.2f}")
-        print('Proceed to payment')
-        paymentMethod(newPrice)
-        return newPrice
-    else:
-        print("Invalid promo code")
-        return applyPromo(user_code,total_price)
-
-
-def paymentMethod(price):
-    print('''Choose a payment method:
-            1. Cash
-            2. Debit/Credit Card ''')
-    payment = int(input('Enter the payment method: '))
-    if payment == 1:
+        print('\nProceed to payment.....')
+        print("\nOrder confirmed! Thank you for your order.")
         print("Please pay at the counter.")
-        print("Order confirmed! Thank you for your order.")
-
-    elif payment == 2:
-        card_number = input("Enter your credit card number: ")
-        cvv_number = input("Enter your cvv pin: ")
-        print("Processing card payment...")
-        print("Payment successful!")
-        print("Order confirmed! Thank you for your order.")
-
+        #ends the loop and the returns the newPrice after promo/no promo code usage
+        return newPrice,user_code
     else:
-        print("Invalid payment method. Please try again.")
-        paymentMethod(price)
+        print("Invalid promo code.Please try again.\n")
+        return applyPromo(total_price)
 
-
+#this function prints out the promo code and its percentage 
 def promoReceipt(user_code,total):
     if user_code == 'NO':
-        print(f"No Promo:\t\t\t\t\t\t\t\t-RM0.00")
+        print(f"{'No Promo':<54}{'-0.00'}")
         return
     percentage = promo[user_code]
     minus = total*(percentage/100)
-    print(f"{user_code}({percentage}% OFF):\t\t\t\t\t\t-RM{minus:.2f}")
+    print(f"{user_code}({percentage}% OFF)\t\t\t\t\t\t\t\t\t\t  -{minus:.2f}")
 
+#function to add new promo code, if the admin wishes to do so
+def addPromo():
+    newPromo = input("Enter new promo code: ").upper()
+    off = int(input("Enter the percentage: "))
+    if newPromo not in promo:
+        promo.update({newPromo:off})
+    else:
+        print("Promo Code exists")
